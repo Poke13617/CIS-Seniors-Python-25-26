40a88e2652d1d2b7d3986491682bb13d438d9b51
Poke13617 <cte-mason.curtis@sllboces.org>
2025-09-16 13:52:56 +0000

    printExpressions

---
+++ printExpressions.py
@@ -0,0 +1,19 @@
+'''
+Class: CIS Seniors 25-26
+Name: Mason Curtis
+Assignment: Printing expressions
+'''
+
+print('Hello, my name is Mason.')
+
+print("My favorite song!") 
+"""
+It's an instrumental song without lyrics
+"""
+print("-----------------")
+print()
+print("The first 4 lines of your song.")
+print("...")
+print("...")
+print("...")
+print("...")
