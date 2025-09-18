918e1a1809c0b44c2e0672e2bba3ef25a24338c8
Poke13617 <cte-mason.curtis@sllboces.org>
2025-09-18 14:31:54 +0000

    ...

---
+++ assignments/Unit 1/socialProfile.py
@@ -0,0 +1,31 @@
+'''
+Social Media Profile
+Description: Create a "Social Media Profile" using variables of all four data types:
+Strings: username, bio, favorite_quote
+Integers: followers, following, posts
+Floats: average_likes_per_post, engagement_rate
+Booleans: verified_account, private_profile
+Name: Mason Curtis
+Date: 9/18/25
+'''
+
+username = "Poke"
+bio = "need something? Speak up!  Leading player of the local HS esports team."
+favorite_quote = "There's always a better deal.  - My father"
+followers = 10
+following = 13
+posts = 10
+average_likes_per_post = 5.0
+engagement_rate = 20.5
+verified_account = False
+private_profile = True
+
+print(username)
+print("bio: " + bio)
+print("favorite quote: " + favorite_quote)
+print("followers: " + str(followers) + "    following: " + str(following))
+print("posts: " + str(posts))
+print("average likes per post: " + str(average_likes_per_post))
+print("engagement rate: " + str(engagement_rate) + "%")
+print("verified: " + str(verified_account))
+print("privated profile? " + str(private_profile))
