9cace08b457239dd7e8d089cf8636453e379d8fa
Poke13617 <cte-mason.curtis@sllboces.org>
2025-09-18 13:26:13 +0000

    ...

---
+++ dreamVacation.py
@@ -0,0 +1,19 @@
+
+'''
+Dream Vacation
+Description: Variable practice.
+Mason Curtis
+09/17/25
+'''
+
+location = "Australia"
+cost = 4000
+duration = "1 week"
+activities = "seeing family"
+travelCompanions = "my mother and brother"
+
+print("The place would likely be " + location)
+print("The cost to go to " + location + " is " + str(cost))
+print("for the price of $" + str(cost) + ", it would last for " + duration)
+print("The activities that I would accomplish during the " + duration + " time period would likely be " + activities)
+print("The only person I would likely bring with me is " + travelCompanions)
---
+++ morePyVars.py
@@ -0,0 +1,26 @@
+'''The Doc String
+Description: Notes about python variables
+Name: Mason Curtis
+Date: 9/18/25
+'''
+# Variable assignment
+firstName = "Mason"
+lastName = "Curtis"
+gradeLevel = 12
+favoriteSubject = "Computer Science"
+yearsInSchool = 12
+age = 18
+print("My name is " + firstName + " " + lastName)
+print("I am in ", gradeLevel, "th grade.")
+print("My favorite subject is " + favoriteSubject)
+print("I have been in school for ", yearsInSchool)
+
+print("\n\n\n")
+print("Mathematical Calculations")
+
+#Storing mathematical values
+length = 15
+width = 8
+height = 12
+
+# calculate are and volume
