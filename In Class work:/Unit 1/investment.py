2d743c30f98937110a248c0ddf2bb00d4db208ce
Poke13617 <cte-mason.curtis@sllboces.org>
2025-09-22 14:14:50 +0000

    ...

---
+++ In Class work:/Unit 1/investment.py
@@ -0,0 +1,40 @@
+'''
+Program: investment.py
+Author: Mason Curtis
+Class: CIS Seniors
+Date: 9/22/25
+Compute an investment report
+1. The inputs are:
+    starting investment amount
+    number of years
+    Interest rate (an integer percent)
+
+    2. The report is displayed in tabular form with a header
+    3. Computations and outputs:
+        for each year of the investment
+            compute the interest and add it to the investment
+            print a formatted row of results for that year
+    4. The ending investment and interest you have paid in total are also displayed
+'''
+
+print("Investment calculator")
+print("=" * 25)
+
+# Accept the inputs
+investStarter = int(input("Enter the starting amount: "))
+yearsTotal = int(input("How many years total are there"))
+interestRate = int(input("Enter the interest rate as a percent"))
+
+# Convert the rate to a decimal number
+decIntRate = interestRate / 100
+print(decIntRate)
+# Initialize the accumulator for the interest
+
+
+# Display the header for the table
+
+
+# Compute and display the results for each year
+
+
+# Display the totals for the period
