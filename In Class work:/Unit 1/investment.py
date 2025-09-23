73eed4c8e33580d4cdc4593fb1416d2ffa6a8288
Poke13617 <cte-mason.curtis@sllboces.org>
2025-09-23 14:19:40 +0000

    investment.py almost done (table is WIP

--- In Class work:/Unit 1/investment.py
+++ In Class work:/Unit 1/investment.py
@@ -19,26 +19,37 @@ Compute an investment report
 
 print("Investment calculator")
 print("=" * 25)
-
+print("")
 # Accept the inputs
 investStarter = int(input("Enter the starting amount: "))
 yearsTotal = int(input("Enter the number of years: "))
 interestRate = int(input("Enter the interest rate as a percent: "))
+investStarter2 = investStarter
 
+print("")
 # Convert the rate to a decimal number
 decIntRate = interestRate / 100
 
-# Initialize the accumulator for the interest
-i = 0
-while i < int(yearsTotal):
-  investStarter = investStarter * decIntRate
-  print( "Year " + str(i) + "--" + str(investStarter))
-  i += 1
-
 # Display the header for the table
 
+print("=" * 40)
+print("What your " + str(yearsTotal) + " year investment of $" + str(investStarter) + " looks like.")
+print("=" * 40)
+print("")
 
-# Compute and display the results for each year
+# Initialize the accumulator for the interest
+i = 0
+print("-" * 40)
+print("||      Year       ||     investment   ")
+
+while i < int(yearsTotal):
+  investStarter2 = investStarter2 * (1 + decIntRate)
+  print("-" * 40)
+  print( "||     Year " + str(i+1) + "      ||      $" + str(round(investStarter2, 2)))
+  i += 1
+print("-" * 40)
+print("\nAfter " + str(yearsTotal) + " Years, your investment of $" + str(investStarter) + " would turn into $" + str(round(investStarter2, 2)))
+# Compute and display the results for each year (above)
 
 
 # Display the totals for the period
