3a449739bde2f55e0b234943f817278379268a8b
Poke13617 <cte-mason.curtis@sllboces.org>
2025-09-25 13:48:07 +0000

    investmentBook.py done

--- In Class work:/Unit 1/investmentBook.py
+++ In Class work:/Unit 1/investmentBook.py
@@ -20,9 +20,9 @@ print("=" * 40)
 print("\n\nMy investment tracker")
 print("=" * 40)
 # Accept the inputs
-startBalance = float(input("Enter the investment amount"))
-years = int(input("Enter the number of years"))
-rate = int(input("Enter the rate as a percent"))
+startBalance = float(input("Enter the investment amount "))
+years = int(input("Enter the number of years "))
+rate = int(input("Enter the rate as a percent "))
 
 # Convert the rate to a decimal number
 rate = rate/100
@@ -36,5 +36,13 @@ print("%4s%18s%10s%16s" % ("Year", "Starting Balance", "Interest", "Ending Balan
 
 # Compute and display results for each year
 
-
+for year in range(1, years + 1):
+    interest = startBalance * rate
+    endBalance = startBalance + interest
+    print("%4d%18.2f%10.2f%16.2f" % (year, startBalance, interest, endBalance))
+    startBalance = endBalance
+    totalInterest += interest
 #Display the totals for the period
+
+print("Ending balance: $%0.2f" % endBalance)
+print("Total interest earned: $%0.2f" % totalInterest)
