b3c8d8ea058df384f97b51896f46987bce81f631
Poke13617 <cte-mason.curtis@sllboces.org>
2025-11-04 14:06:57 +0000

    ...

---
+++ In Class work:/Unit 3/pyDict.py
@@ -0,0 +1,36 @@
+'''
+
+program: pyDict.py
+Author: Mason Curtis
+Date: 11/04/2025
+
+- Student ID: "S12345"
+- Grade Level: 12
+- Email: "emma.r@school.edu"
+- Math grade: 95
+- English Grade: 88
+- Science grade: 92
+- Homework completed - true
+
+Tasks:
+1. Create a dictionary called student1 with all the information above
+2. print the entire dictionary
+3. Print just emma's email address
+4. Print just Emma's math grade
+'''
+
+# Part 1: Create your dictionary here
+student1 = {"name": "Emma Rodriguez",
+            "student_id": "S12345",
+            "grade_level": 12,
+            "email": "emma.r@school.edu",
+            "math_grade": 95,
+            "english_grade": 88,
+            "science_grade": 92,
+            "homework_completed": True
+            }
+
+# Print the entire dictionary
+print(student1)
+
+# Print specific values
