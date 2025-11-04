9511ada1d82bad25756319ce180066019b3597a7
Poke13617 <cte-mason.curtis@sllboces.org>
2025-11-04 15:28:27 +0000

    ...

--- In Class work:/Unit 3/pyDict.py
+++ In Class work:/Unit 3/pyDict.py
@@ -31,6 +31,104 @@ student1 = {"name": "Emma Rodriguez",
             }
 
 # Print the entire dictionary
+print("Complete student record:")
 print(student1)
 
 # Print specific values
+print("\nStudent email:", student1["email"])
+print("Math grade:", student1["math_grade"])
+
+# Part 2
+
+# Modify her homework status
+student1["homework_completed"] = False
+
+# Update her english grade
+student1["english_grade"] = 91
+
+# Add a history grade
+student1["history_grade"] = 89
+
+# Add clubs information
+student1["clubs"] = ["Debate Team", "Math Club"]
+
+#Print the updated directory
+print("\nUpdated student record:")
+print(student1)
+
+
+# Part 3: Create a function to calculate average grade
+#Python function structure: def(keyword) func_name(argument)
+def calculate_average(student):
+
+    '''
+    this function calculates the student average
+    '''
+
+    # Get all the grades
+    math = student1["math_grade"]
+    science = student1["science_grade"]
+    history = student1["history_grade"]
+    english = student1["english_grade"]
+    
+
+    # Calculate the average
+    average_num = (math + history + english + science) / 4
+
+    # Return the average
+    return average_num
+    
+average = calculate_average(student1)
+print(f"\n{student1['name']}'s average grade: {average:.2f}")
+
+
+# Part 4: Using dictionary methods
+'''
+.keys(), .values(), .items(), .get(), .update()
+'''
+
+# Print all keys in the dictionary
+student_keys = student1.keys()
+print("\nAll values in the dictionary:", student_keys)
+# Print all values
+student_values = student1.values()
+print("\nAll values in the dictionary:", student_values)
+#print(student1.values)
+
+# Print all key value pairs
+student_items = student1.items()
+print("\nAll the key-value pairs:", student_items)
+for key, value in student1.items():
+    print(f"{key}: {value}")
+#print(student1.items())
+
+#Safely get phone number (doesn't exist)
+phoneNum = "###-###-####"
+student1.update({"phone_number": phoneNum})
+phone = student1.get("phone_number")
+# student1.get("phone_number", "not available")
+
+print("\n Phone number:", phone)
+
+
+#
+student2 = {
+    "name": "Marcus Chen",
+    "student_id": "S123456"
+}
+
+
+# Update grade to 11, math to 87, ela to 90, and science to 85
+
+student2.update({
+    "grade_level": 11,
+    "math_grade": 87,
+    "english_grade": 90,
+    "science_grade": 85
+})
+#
+
+print("\nNew student information:", student2)
+#
+
+
