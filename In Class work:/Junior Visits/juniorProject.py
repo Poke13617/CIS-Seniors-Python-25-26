bb8b7c07352c78731e93a41bfe4987c543b764ee
Poke13617 <cte-mason.curtis@sllboces.org>
2026-02-02 14:58:24 +0000

    ...

---
+++ In Class work:/Junior Visits/juniorProject.py
@@ -0,0 +1,46 @@
+# About Me Profile Creator
+# Created by: ___________________
+
+print("=== ABOUT ME PROFILE ===")
+
+# TODO: Ask for the user's name and store it in a variable called 'name'
+
+name = input("what is your name? ")
+
+
+# TODO: Ask for their grade and store it in a variable called 'grade'
+
+grade = int(input("what is your grade"))
+
+# TODO: Ask for their favorite subject (you pick the variable name!)
+
+subject = input("what is your favorite subject?")
+
+# TODO: Ask for their hobby (you pick the variable name!)
+
+hobby = input()
+
+# TODO: Ask for their dream job (you pick the variable name!)
+
+
+# TODO: Display the profile below
+print("")
+print("=" * 40)
+print("PROFILE FOR " + name.upper())
+print("=" * 40)
+
+# TODO: Print the grade
+
+
+# TODO: Print the favorite subject
+
+
+# TODO: Print the hobby
+
+
+# TODO: Print the dream job
+
+
+# TODO: Add a personalized message using their name
+
+print("=" * 40)
